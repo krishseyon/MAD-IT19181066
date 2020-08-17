1e6bb707cc40cf628096259331a603a2fcd8852e
ASUS <krishseyon01@gmail.com>
2020-08-17 10:57:01 +0530

    IT19181066 seyon.k

---
+++ app/src/main/java/com/example/myfinance/UpdateCredit.java
@@ -0,0 +1,14 @@
+package com.example.myfinance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateCredit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_credit);
+    }
+}
---
+++ app/src/main/java/com/example/myfinance/updatedebit.java
@@ -0,0 +1,14 @@
+package com.example.myfinance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class updatedebit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatedebit);
+    }
+}
